fd38cbf0053455a6dd2431b1eeebfd3981cd0833
Alexander Daily <alex@cozero.io>
2022-08-30 18:56:50 +0200

    refactor

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,35 @@
 import { emitKeypressEvents } from 'readline'
 import Audic from 'audic'
 
-
-const THRESHOLD = 50
-
 async function main() {
-  const player = new Audic('./test.mp3')
-  let timeSinceLastPress = 0
-  let lastpressed: string = ''
-
+  // setup
   emitKeypressEvents(process.stdin)
   process.stdin.setRawMode(true)
-
+  const player = new Audic('./test.mp3')
+  await new Promise(_ => setTimeout(_, 1000))
+  console.log('start playing')
+  // key event handler
   process.stdin.on('keypress', (_, key) => {
     if (key.ctrl && key.name === 'c') {
       player.destroy()
       return process.exit()
     }
-
     if (key.name === lastpressed) return
 
     lastpressed = key.name
     timeSinceLastPress = 0
-    if (!player.playing) {
-      player.play()
-    }
+
+    if (!player.playing) player.play()
   })
 
+  // key variables
+  let timeSinceLastPress = 0
+  let lastpressed: string = ''
+
+  // event loop
   setInterval(() => {
     timeSinceLastPress += 10
-    if (timeSinceLastPress > THRESHOLD) {
-      player.pause()
-    }
+    if (timeSinceLastPress > 50) player.pause()
   }, 75)
 }
 
